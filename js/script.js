575ae59bc113cc2b1a11ef72f75c6d610d59aab3
William Fischdick <jwfbit@hotmail.com>
2023-05-07 17:12:41 -0300

    :construction: feat: Fazendo validação do CPF e Idade do usuário.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 import umCpf from "./valida-cpf.js";
+import maiorDeIdade from "./valida-idade.js";
 
 const camposDoFormulario = document.querySelectorAll("[required]");
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
+    campo.addEventListener("invalid", evento => evento.preventDefault());
 })
 
 function verificaCampo(campo) {
-    if (campo.name == "cpf" && campo.value.lenght >=11) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
         umCpf(campo);
+    }
+
+    if (campo.name == "aniversario" && campo.value !="") {
+        maiorDeIdade(campo);
     }
 }
--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,21 +1,63 @@
 export default function umCpf(campo) {
     const cpf =  campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf);
+    if(validaNumerosRepetidos(cpf) || validarPrimeiroDigito(cpf) || validarSegundoDigito(cpf)) {
+        alert("CPF inválido");
+    } else {
+        console.log("cpf válido");
+    }
 }
 
 function validaNumerosRepetidos(cpf) {
-    const numeroRepetidos =[
-        "00000000000",
-        "11111111111",
-        "22222222222",
-        "33333333333",
-        "44444444444",
-        "55555555555",
-        "66666666666",
-        "77777777777",
-        "88888888888",
-        "99999999999",
+    const numeroRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
     ]
 
     return numeroRepetidos.includes(cpf);
+}
+
+/* Validação de CPF este cálculo serve para garantir que o cpf é válido*/
+
+function validarPrimeiroDigito(cpf) {
+    let soma = 0
+    let multiplicador = 10;
+
+    for(let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+    
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[9];
+}
+
+function validarSegundoDigito(cpf) {
+    let soma = 0
+    let multiplicador = 11;
+
+    for(let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--;
+    }
+    
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
